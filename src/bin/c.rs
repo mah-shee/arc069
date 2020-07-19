a8bd2315214413f9c631f3f000c66ca02e728e97
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-19 14:11:59 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,13 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s: usize,
+        c: usize,
+    }
+    if s * 2 >= c {
+        println!("{}", c / 2);
+    } else {
+        let rest = s + (c - s * 2) / 4;
+        println!("{}", rest);
     }
 }
